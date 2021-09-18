b28ce4eb9142932e045384b8253d5100430a1216
JewHaHa <saran_p600@hotmail.com>
2021-09-18 19:29:37 +0700

    first commit

---
+++ vue/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+      <Header />
+    </div>
+</template>
+
+<script>
+import  Header from "./components/Header.vue";
+export default {
+  components: {
+    Header,
+  }
+}
+</script>
